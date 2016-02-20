673d07e10dc2691315c2fda357076a0ce8698ac2
robinfwzdev <robin@futureworkz.com>
2016-02-20 10:57:21 +0700

    Create data seed for husbandship

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,4 +20,5 @@ sofia   = Person.create!(first_name: 'Sofia',   last_name: 'Fwz', dob: 15.years.
   Mothership.create(person: person, member: lily)
 end
 
-john = Wifeship.create(person: john, member: lily)
+Wifeship.create(person: john, member: lily)
+Husbandship.create(person: lily, member: john)
